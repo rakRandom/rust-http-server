c40676874c64650e7963c7b2e13f717b8232fb38
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-08-09 02:19:04 -0300

    feat: add 404 page

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,34 @@ use std::{
 };
 
 fn handle_connection(mut stream: TcpStream) {
+    //
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {http_request:#?}");
+    //
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "static/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("static/index.html").unwrap();
-    let length = contents.len();
+    //
+    let contents: String = fs::read_to_string(filename).unwrap();
+    let length: usize = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
+    //
     stream.write_all(response.as_bytes()).unwrap();
 
     println!("Response: {response}");
 }
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream: TcpStream = stream.unwrap();
 
         handle_connection(stream);
     }
