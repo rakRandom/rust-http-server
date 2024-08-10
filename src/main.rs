1bcb570836d768931f187cbe0118791a71664c45
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-08-10 20:24:08 -0300

    feat: improve request parsing and prevent bugs

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    //
+    // Getting http request body
     let buf_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
 
     let http_request: Vec<_> = buf_reader
@@ -30,51 +30,63 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    if http_request.len() == 0 {
-        return;
-    }
+    if http_request.len() == 0 { return; }  // No body request error
 
     let request_line: String = http_request[0].clone();
 
     println!("Request {:?}: {http_request:#?}\r\n", chrono::offset::Local::now());
 
-    //
-    let (status_line, filename) = 
-    match request_line.as_str() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/index.html"),
-        "GET /file HTTP/1.1" => ("HTTP/1.1 200 OK", "static/file.txt"),
-        &_ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
-    };
+    // Parsing request
+    let temp: Vec<&str> = request_line.split(' ').collect::<Vec<_>>();
+    if temp.len() < 1 { return; }  // No request line error
 
-    if filename == "static/file.txt" {
-        send_file(stream, status_line, filename);
-        return;
+    let path: &str = temp[1];
+
+    if path.contains('.') {
+        send_file(stream, &path[1..path.len()]);
     }
-
-    //
-    let contents: String = fs::read_to_string(filename).unwrap();
-    let length: usize = contents.len();
-
-    let response: String = 
-    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    //
-    stream.write_all(response.as_bytes()).unwrap();
-
-    println!("Response: {response}\r\n\r\n");
+    else {
+        render_template(
+            stream, 
+            match request_line.as_str() {
+            "GET / HTTP/1.1" => "index.html",
+            &_ => "404.html",
+        });
+    }
 }
 
-fn send_file(mut stream: TcpStream, status_line: &str, filename: &str) {
-    let buf_content: Vec<u8> = fs::read(filename).unwrap();
-    let length: usize = buf_content.len();
-    let filename: &str = filename.split('/').collect::<Vec<_>>()[1];
+fn send_file(mut stream: TcpStream, filename: &str) {
+    let response: String;
 
-    let response = format!("{status_line}\r\n\
-    Content-Disposition: attachment; filename=\"{filename}\"\r\n\
-    Content-Type: text/plain\r\n\
-    Content-Length: {length}\r\n\r\n");
+    match fs::read(format!("static/{filename}")) {
+        Ok(buf_content) => {
+            let length: usize = buf_content.len();
 
+            response = format!("HTTP/1.1 200 OK\r\n\
+            Content-Disposition: attachment; filename=\"{filename}\"\r\n\
+            Content-Type: text/plain\r\n\
+            Content-Length: {length}\r\n\r\n");
+            stream.write_all(response.as_bytes()).unwrap();
+            stream.write_all(&buf_content).unwrap();
+            stream.flush().unwrap();
+        }, 
+        Err(_) => {
+            response = format!("HTTP/1.1 404 FILE NOT FOUND\r\n");
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    };
+}
+
+fn render_template(mut stream: TcpStream, filename: &str) {
+    // Parsing response
+    let status_line = "HTTP/1.1 200 OK";
+    let contents: String = fs::read_to_string(format!("static/{filename}")).unwrap();
+    let length: usize = contents.len();
+
+    let response: String = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    // Sending response
     stream.write_all(response.as_bytes()).unwrap();
-    stream.write_all(&buf_content).unwrap();
-    stream.flush().unwrap();
+    println!("Response: {response}\r\n\r\n");
 }
