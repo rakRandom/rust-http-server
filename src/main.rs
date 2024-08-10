2a3d60168f96076dd57d3d734f73671b5c935a7c
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-08-10 19:41:33 -0300

    feat: add first version of file transfer

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,26 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
+    if http_request.len() == 0 {
+        return;
+    }
+
     let request_line: String = http_request[0].clone();
 
     println!("Request {:?}: {http_request:#?}\r\n", chrono::offset::Local::now());
 
     //
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "static/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    let (status_line, filename) = 
+    match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/index.html"),
+        "GET /file HTTP/1.1" => ("HTTP/1.1 200 OK", "static/file.txt"),
+        &_ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
     };
+
+    if filename == "static/file.txt" {
+        send_file(stream, status_line, filename);
+        return;
+    }
 
     //
     let contents: String = fs::read_to_string(filename).unwrap();
@@ -52,4 +62,19 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write_all(response.as_bytes()).unwrap();
 
     println!("Response: {response}\r\n\r\n");
+}
+
+fn send_file(mut stream: TcpStream, status_line: &str, filename: &str) {
+    let buf_content: Vec<u8> = fs::read(filename).unwrap();
+    let length: usize = buf_content.len();
+    let filename: &str = filename.split('/').collect::<Vec<_>>()[1];
+
+    let response = format!("{status_line}\r\n\
+    Content-Disposition: attachment; filename=\"{filename}\"\r\n\
+    Content-Type: text/plain\r\n\
+    Content-Length: {length}\r\n\r\n");
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(&buf_content).unwrap();
+    stream.flush().unwrap();
 }
